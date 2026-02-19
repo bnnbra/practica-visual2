9dc04a5584fcb9c6520628d21d66feb1310a6166
Brahim Benmouna <163146496+bnnbra@users.noreply.github.com>
2026-02-20 00:59:49 +0100

    Update iva.py

--- iva.py
+++ iva.py
@@ -1,4 +1,4 @@
-precio = 100
-iva = 0.21
-total = precio * (1 + iva)
-print(f"El total con IVA es: {total}")
+def calcular_iva(precio, impuesto=0.21):
+    return precio * (1 + impuesto)
+
+print(f"Total: {calcular_iva(100)}")
